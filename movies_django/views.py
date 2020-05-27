7435d0be3cc6a115505fc130bb64a3e8b0004304
rauan-trustylabs <62379653+rauan-trustylabs@users.noreply.github.com>
2020-05-27 09:58:04 +0600

    add comments to views

--- movies_django/views.py
+++ movies_django/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+# данный модуль отвечает за то что видит клиент когда делает запрос на твой сайт
+# ты можешь прописать что будет на странице. на пример на странице /movies ты должен выставить список фильмов
 def index(request):
     return HttpResponse('Hello, welcome to the index page.')
 
