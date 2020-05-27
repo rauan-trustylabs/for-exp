994e096296d189897361d8bc3df84cbe9bd7c402
rauan-trustylabs <62379653+rauan-trustylabs@users.noreply.github.com>
2020-05-27 09:52:50 +0600

    add comments to settings

--- untitled1_django/settings.py
+++ untitled1_django/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'  # чтобы система понимала русский тебе надо прописать 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC' # чтобы в системе было правильное время нужно прописать 'Asia/Almaty'
 
 USE_I18N = True
 
